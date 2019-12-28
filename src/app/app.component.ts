f765e170ff3b3f5c9b6b9c66872a1eec99fd25ca
Roshan Lakmal <roshanlakmal@Roshans-MacBook-Air.local>
2019-12-28 12:58:32 +1100

    my solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  showParagraph = "none";
+  myLog = [];
+  count = 1;
+  onDisplay(){
+    // if(this.showParagraph === true){
+    //   this.showParagraph = false; 
+    // }else{
+    //   this.showParagraph = true;
+    // } 
+    if(this.showParagraph === "none"){
+      this.showParagraph = "block"; 
+    }else{
+      this.showParagraph = "none";
+    }
+    this.myLog.push(this.count);
+    this.count++; 
+    console.log(this.showParagraph);
+    console.log(this.myLog);
+  }
+  onSetStyle(){
+    
+  }
 }
