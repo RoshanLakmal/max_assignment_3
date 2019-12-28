3906ec6e6e0f7972c030a093eb05deea90409956
Roshan Lakmal <roshanlakmal@Roshans-MacBook-Air.local>
2019-12-28 13:15:32 +1100

    max solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,26 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  showParagraph = "none";
-  myLog = [];
-  count = 1;
-  onDisplay(){
-    // if(this.showParagraph === true){
-    //   this.showParagraph = false; 
-    // }else{
-    //   this.showParagraph = true;
-    // } 
-    if(this.showParagraph === "none"){
-      this.showParagraph = "block"; 
-    }else{
-      this.showParagraph = "none";
-    }
-    this.myLog.push(this.count);
-    this.count++; 
-    console.log(this.showParagraph);
-    console.log(this.myLog);
-  }
-  onSetStyle(){
-    
-  }
+showSecret = false;
+myLog = [];
+onToggleDisplay(){
+  this.showSecret = !this.showSecret;
+  this.myLog.push(this.myLog.length + 1);
+}
+  // showParagraph = "none";
+  // myLog = [];
+  // count = 1;
+  // onDisplay(){
+  //   if(this.showParagraph === "none"){
+  //     this.showParagraph = "block"; 
+  //   }else{
+  //     this.showParagraph = "none";
+  //   }
+  //   this.myLog.push(this.count);
+  //   this.count++; 
+  //   console.log(this.showParagraph);
+  //   console.log(this.myLog);
+  // }
 }
